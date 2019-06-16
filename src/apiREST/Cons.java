b6be5a10c838cbbf325e7725d1a546b4d2f23b59
albertsanchezf <asanchezfuster@gmail.com>
2019-06-17 00:40:00 +0200

    Initial Commit

---
+++ src/apiREST/Cons.java
@@ -0,0 +1,13 @@
+package apiREST;
+
+/**
+ *
+ * @author juanluis
+ */
+public interface Cons 
+{
+  
+  String SERVER_REST = "http://localhost:24872/InstantMessagingRemote_server/rest";
+  String SERVER_WEBSOCKET = "ws://localhost:24872/InstantMessagingRemote_server/ws";
+  
+}
---
+++ src/topicmanager/TopicManagerStub.java
@@ -0,0 +1,74 @@
+package topicmanager;
+
+import apiREST.apiREST_TopicManager;
+import util.Subscription_check;
+import util.Topic;
+import util.Topic_check;
+import java.util.List;
+import publisher.Publisher;
+import publisher.PublisherStub;
+import subscriber.Subscriber;
+import webSocketService.WebSocketClient;
+
+public class TopicManagerStub implements TopicManager 
+{
+
+  public String user;
+
+  public TopicManagerStub(String user) 
+  {
+    WebSocketClient.newInstance();
+    this.user = user;
+  }
+
+  public void close() 
+  {
+    WebSocketClient.close();
+  }
+
+  @Override
+  public Publisher addPublisherToTopic(Topic topic) 
+  {
+    Publisher p = new PublisherStub(topic);
+    apiREST_TopicManager.addPublisherToTopic(topic);
+    return p;
+  }
+
+  @Override
+  public void removePublisherFromTopic(Topic topic) 
+  {
+    apiREST_TopicManager.removePublisherFromTopic(topic);
+  }
+
+  @Override
+  public Topic_check isTopic(Topic topic) 
+  {
+    return apiREST_TopicManager.isTopic(topic);
+  }
+
+  @Override
+  public List<Topic> topics() 
+  {
+    return apiREST_TopicManager.topics();
+  }
+
+  @Override
+  public Subscription_check subscribe(Topic topic, Subscriber subscriber) 
+  {
+    if (apiREST_TopicManager.isTopic(topic).isOpen)
+    {
+        WebSocketClient.addSubscriber(topic, subscriber);
+        return new Subscription_check(topic,Subscription_check.Result.OKAY);
+    }
+    else
+        return new Subscription_check(topic,Subscription_check.Result.NO_TOPIC);     
+  }
+
+  @Override
+  public Subscription_check unsubscribe(Topic topic, Subscriber subscriber) 
+  {
+    WebSocketClient.removeSubscriber(topic);
+    return null;
+  }
+
+}
